634021e60d9fa5c5766429b9c2a31ea4a06556d9
Darlenys Gomez <darlenysgz@gmail.com>
2018-09-12 21:35:32 -0400

    Lista de videos y detalles del actual

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from '../node_modules/youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'; 
 //Create a new component that should produce some HTML.
 
 const API_KEY = 'AIzaSyAN-BA8iP5ia2DU754dtLrCvIH-a5uvhHI';
 
-const App = () => {
-	return (
+class App extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = { videos : [] };
+
+		YTSearch({key: API_KEY, term: 'wines'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render(){
+		return (
 		<div>
 			<SearchBar/>
+			<VideoDetail video={this.state.videos[1]}/>
+			<VideoList videos={this.state.videos}/>
 		</div>
 
 	);
+	}
+	
 }
 
 ///Take the component and put in the dom
